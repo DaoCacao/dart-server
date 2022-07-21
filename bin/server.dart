a5ec2c0af9b31376c9b1f6b65f4f67b087d240b4
daocacao <mrdaocacao@gmail.com>
2022-07-21 21:49:34 +0400

    add CORS

--- bin/server.dart
+++ bin/server.dart
@@ -94,10 +94,33 @@ void main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final _handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final _handler = Pipeline()
+      .addMiddleware(fixCORS())
+      .addMiddleware(logRequests())
+      .addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment["PORT"] ?? "8080");
   final server = await serve(_handler, ip, port);
   print("Server listening on http://${server.address.host}:${server.port}");
+}
+
+Middleware fixCORS() {
+  const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': '*',
+  };
+  return createMiddleware(
+    requestHandler: (Request request) {
+      if (request.method == 'OPTIONS') {
+        return Response.ok(null, headers: corsHeaders);
+      } else {
+        return null;
+      }
+    },
+    responseHandler: (Response response) {
+      return response.change(headers: corsHeaders);
+    },
+  );
 }
